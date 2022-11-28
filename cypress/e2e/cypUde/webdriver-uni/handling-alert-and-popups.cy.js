630df38042b7618377e83f28b9c442f3167a307b
emildev666 <emilg.242@gmail.com>
2022-11-29 00:33:29 +0400

    handling alerts DONE!

--- cypress/e2e/cypUde/webdriver-uni/handling-alert-and-popups.cy.js
+++ cypress/e2e/cypUde/webdriver-uni/handling-alert-and-popups.cy.js
@@ -1,15 +1,39 @@
 ///<reference types='cypress'/>
 
 describe('Handle js alers', () => {
-    it('Confirm js alerts contain the correct text', () => {
+    beforeEach(() => {
         cy.visit('http://www.webdriveruniversity.com/');
-        cy.get('#popup-alerts').invoke('removeAttr', 'target').click();
-
-        cy.get('#button1').click();
-        cy.on('window:alert' , (param)=>{
-            expect(param).to.eq('I am an alert box!')
-        } )
     });
+    it('Confirm js alerts contain the correct text', () => {
+        cy.get('#popup-alerts').invoke('removeAttr', 'target').click();
+        cy.get('#button1').click();
+        cy.on('window:alert', (param) => {
+            expect(param).to.eq('I am an alert box!')
+        })
+    });
+    it('Validate js confirm alert box  works corrextly when clicking Ok!', () => {
+        cy.get('#popup-alerts').invoke('removeAttr', 'target').click();
+        cy.get('#button4').click();
+        cy.on('window:confirm', (param) => {
+            return true //it'll click to OK.But if we return FALSE it'll click to CANCEL 
+        })
+        cy.get('#confirm-alert-text').contains('You pressed OK!')   
+        // OR
+        // cy.get('#confirm-alert-text').then(obj=>{
+        //     expect(obj.text()).to.eq('You pressed OK!')
+        // })
+
+    });
+    it('Validate js confirm alert box  works corrextly when clicking cancel', () => {
+        cy.get('#popup-alerts').invoke('removeAttr', 'target').click({ force: true });
+        cy.get('#button4').click();
+        cy.on('window:confirm', (param) => {
+            return false;
+        })
+        cy.get('#confirm-alert-text').contains('You pressed Cancel!')
+    });
+    
+
 });
 
 

5673438e3cc2a6bbc9333e3efc58e5727589825a
emildev666 <emilg.242@gmail.com>
2022-12-01 17:04:52 +0400

    check spesific radio buttons

---
+++ cypress/e2e/cypUde/webdriver-uni/radioButtons.cy.js
@@ -0,0 +1,18 @@
+///<reference types='cypress'/>
+
+describe('Verify radio buttons via  webdriverUni', () => {
+    beforeEach(() => {
+        cy.visit('http://www.webdriveruniversity.com/');
+    });
+    it('check spesific radio buttons', () => {
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click();
+        cy.get('#radio-buttons').find("input[type='radio']").as('radioButtons')
+        cy.get('@radioButtons').each((el, i) => {
+            cy.wrap(el).as('wrappedRadioButtons')
+            // cy.log('@wrappedRadioButtons')
+            cy.get('@wrappedRadioButtons').check().should('be.checked')
+        })
+    });
+});
+
+

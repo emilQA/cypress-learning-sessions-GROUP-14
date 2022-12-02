7b6a5897ff0e004a17942cd9bf935d952572be55
emildev666 <emilg.242@gmail.com>
2022-12-02 17:25:05 +0400

    Select the spesific values via select dropdown list

---
+++ cypress/e2e/cypUde/webdriver-uni/dropdowns.cy.js
@@ -0,0 +1,42 @@
+///<reference types='cypress'/>
+
+describe('Interract with dropdown lists via webdriveruni', () => {
+    beforeEach(() => {
+        cy.visit('http://www.webdriveruniversity.com/');
+    });
+    it('Select the spesific values via select dropdown list', () => {
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click();
+        cy.get('#dropdowm-menu-1').as('mainDropdown1')
+        cy.get('#dropdowm-menu-1 > option').as('dropdownLists1')
+        cy.get('@dropdownLists1').each((el) => {
+            cy.get('@mainDropdown1').select(el.text()).should('have.value', el.text().toLocaleLowerCase())
+        })
+        cy.get('#dropdowm-menu-2').as('mainDropdown2')
+        cy.get('#dropdowm-menu-2 > option').as('dropdownLists2')
+        cy.get('@dropdownLists2').each((el) => {
+            cy.get('@mainDropdown2').select(el.text()).should('have.value', el.text().toLocaleLowerCase())
+        })
+        cy.get('#dropdowm-menu-1').as('mainDropdown')
+        cy.get('#dropdowm-menu-1 > option').as('dropdownLists')
+        cy.get('@dropdownLists').each((el) => {
+            cy.get('@mainDropdown').select(el.text()).should('have.value', el.text().toLocaleLowerCase())
+        })
+    });
+
+
+    
+    it.only('DROP', () => {
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click();
+        cy.get('.dropdown-menu-lists').as('dropdownMenuLists');
+        cy.get('@dropdownMenuLists').each((el, i) => {
+            if (cy.get('@dropdownMenuLists').should('have.id', `#dropdowm-menu-${i+1}`)) {
+                cy.get(`#dropdowm-menu-${i + 1} > option`).as('dropdownMenuListsOption');
+                cy.get('@dropdownMenuListsOption').each((elem) => {
+                    cy.get('@dropdownMenuLists').select(el.text())
+                })
+            }
+        })
+    });
+});
+
+
--- cypress/e2e/cypUde/webdriver-uni/radioButtons.cy.js
+++ cypress/e2e/cypUde/webdriver-uni/radioButtons.cy.js
@@ -13,6 +13,11 @@ describe('Verify radio buttons via  webdriverUni', () => {
             cy.get('@wrappedRadioButtons').check().should('be.checked')
         })
     });
+    it('Validate the state of spesific radio buttons', () => {
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click();
+        cy.get('#radio-buttons').find("input[type='radio']").as('radioButtons')
+       
+    });
 });
 
 
